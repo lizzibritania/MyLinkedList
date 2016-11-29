5e1809b44ceaf39d45d17c3262af1bc59661a7b0
lizzibritania <elizbeth@li.ru>
2016-11-30 00:55:16 +0300

    1

--- src/TestUtil.java
+++ src/TestUtil.java
@@ -1,47 +1,39 @@
 /**
- * Created by Lizzi on 29.11.2016.
+ * Created by Юля on 29.11.2016.
  */
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
+import java.util.*;
 public class TestUtil {
-    public TestUtil() {
-    }
-
     public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
-        LinkedList linkedList = new LinkedList();
+
+
+        ArrayList arrayList=new ArrayList();
+        LinkedList linkedList=new LinkedList();
+
         MyPoint myPoint1 = new MyPoint(1, 2);
         System.out.println("Estimated time for add(Object element) method of java.itil.ArrayList");
         long startTime = System.nanoTime();
-
-        for(int estimatedTime = 0; estimatedTime < 100000; ++estimatedTime) {
+        for (int i = 0; i < 100000; i++) {
             arrayList.add(myPoint1);
         }
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.println(estimatedTime);
 
-        long var16 = System.nanoTime() - startTime;
-        System.out.println(var16);
         System.out.println("Estimated time for add(Object element) method of java.itil.LinkedList");
         long startTime1 = System.nanoTime();
-
-        for(int estimatedTime1 = 0; estimatedTime1 < 100000; ++estimatedTime1) {
+        for (int i = 0; i < 100000; i++) {
             linkedList.add(myPoint1);
         }
+        long estimatedTime1 = System.nanoTime() - startTime1;
+        System.out.println(estimatedTime1);
 
-        long var17 = System.nanoTime() - startTime1;
-        System.out.println(var17);
         long startTime2 = System.nanoTime();
-
-        for(int estimatedTime2 = 0; estimatedTime2 < 100000; ++estimatedTime2) {
+        for (int i = 0; i < 100000; i++) {
             linkedList.add(myPoint1);
         }
-
-        long var18 = System.nanoTime() - startTime2;
-        System.out.println(var18);
+        long estimatedTime2 = System.nanoTime() - startTime2;
+        System.out.println(estimatedTime2);
     }
+
+
+
 }
